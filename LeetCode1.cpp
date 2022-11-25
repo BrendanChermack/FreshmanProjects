acfa3da8444af5bb037956257a50c8b9b8774cd3
Brendan Chermack <101033479+BrendanChermack@users.noreply.github.com>
2022-11-25 01:04:54 -0600

    Update LeetCode1.cpp

--- LeetCode1.cpp
+++ LeetCode1.cpp
@@ -22,6 +22,15 @@ public:
         }
         return answers;
     }
+ //solution 2 HASHMAP python
+    def twoSum(self, nums: List[int],target: int) -> List[int]:
+        prevMap = {} //# val and index using enumerate aka keeps track of hte index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+    
     
     int main()
     {
